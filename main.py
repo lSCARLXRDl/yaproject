978086b292976f81d7af1b281e604afeddab5e5d
lSCARLXRDl <serega.tetov@gmail.com>
2020-12-15 19:16:00 +0300

    v3000

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QMenuBar, QToolBar, QComboBox, QLabel, QAction
+from PyQt5.QtWidgets import QColorDialog
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5 import QtWidgets
 
@@ -12,15 +13,28 @@ class Example(QWidget):
     def initUI(self):
         self.setGeometry(300, 300, 700, 500)
         self.setWindowTitle('Paint')
+        self.setStyleSheet("background-color: #c8d9cb;")
+        self.color = '#000000'
 
         self.menubar = QMenuBar(self)
         self.menubar.addMenu('Файл')
         self.menubar.addMenu('Главная')
         self.menubar.addMenu('Вид')
+        self.menubar.setStyleSheet("background-color: white;")
 
         self.toolbar = QToolBar(self)
-        self.toolbar.move(0, 30)
-        icons = QtWidgets.QFileIconProvider()
+        self.toolbar.move(0, 25)
+
+        self.lb = QLabel(self)
+        self.lb.resize(330, 43)
+        self.lb.move(377, 25)
+        self.lb.setStyleSheet("background-color: white;")
+
+        self.paint_lbl = QLabel(self)
+        self.paint_lbl.resize(650, 400)
+        self.paint_lbl.move(20, 80)
+        self.paint_lbl.setStyleSheet("background-color: white; border-style: solid;"
+                                     " border-width: 2px; border-color: black;")
 
         self.cb = QComboBox(self)
         self.cb.setFixedSize(50, 30)
@@ -38,6 +52,24 @@ class Example(QWidget):
         self.toolbar.addAction(QIcon('venv/fill.ico'), 'Fill')
         self.toolbar.addAction(QIcon('venv/text.ico'), 'Text')
         self.toolbar.addSeparator()
+
+        self.lbl = QLabel()
+        self.lbl.setFixedSize(30, 30)
+        self.lbl.setStyleSheet("background-color: black; border-style: solid; border-width: 2px; border-color: black;")
+        self.toolbar.addWidget(self.lbl)
+
+        a = self.toolbar.addAction(QIcon('venv/color.ico'), 'Select color')
+        a.triggered.connect(self.click1)
+
+        self.toolbar.setStyleSheet("background-color: white;")
+
+    def click1(self):
+        color = QColorDialog.getColor()
+        if color.isValid():
+            self.lbl.setStyleSheet(
+                f"background-color: {color.name()}; border-style: solid; border-width: 2px; border-color: black;")
+            self.color = color.name()
+            print(self.color)
 
 
 if __name__ == '__main__':
